8763e09f0768279c3a9518e359996188095dc686
aaaaadrien <aaaaadrien@users.noreply.github.com>
2016-08-24 19:46:32 +0200

    Mise à jour de la configutaion le 24-08-2016 à 19:46:32

--- sauvegarde.sh
+++ sauvegarde.sh
@@ -5,7 +5,7 @@ path=`dirname $0`
 cd "$path"
 
 # On défini les variables
-#gitrepo="git@github.com:aaaaadrien/css-config.git"
+#gitrepo="git@github.com:aaaaadrien/cld-config.git"
 package="etc/portage/package."
 jour=$(date +"%d-%m-%Y")
 heure=$(date +"%H:%M:%S")
@@ -42,6 +42,19 @@ then
 	fi
 fi
 
+
+chemin="etc/calculate/" 
+if [ -f "/$chemin/calculate.env" ]
+then
+        echo "On sauvegarde les préférences du système calculate..." 
+	if [ -d $chemin ]
+	then
+		cp "/$chemin/calculate.env" "$chemin/calculate.env"
+	else
+		mkdir -p "$chemin"
+		cp "/$chemin/calculate.env" "$chemin/calculate.env"
+	fi
+fi
 
 git add *
 git commit -m "Mise à jour de la configutaion le $jour à $heure"
